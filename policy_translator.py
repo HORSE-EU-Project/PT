0c0dc0b1e36cc7c767486c5054e8dfd764a085ad
alonsoumu <alonso.sanchezg1@um.es>
2024-09-11 11:33:37 +0200

    Update policy_translator.py /api and /send_policy

--- policy_translator.py
+++ policy_translator.py
@@ -1,59 +1,95 @@
 from flask import Flask, request, jsonify, Response
 import xml.etree.ElementTree as ET
 import requests
+import json
 
 app = Flask(__name__)
 
 def json_to_xml(json_data):
-    itresource_orchestration = ET.Element("ITResourceOrchestration", 
-                                          xmlns="http://modeliosoft/xsddesigner/a22bd60b-ee3d-425c-8618-beb6a854051a/ITResource.xsd",
-                                          attrib={
-                                              'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
-                                              'id': "omspl_5b0fd7359a5e40fe92399710c1152ed9",
-                                              'xsi:schemaLocation': "http://modeliosoft/xsddesigner/a22bd60b-ee3d-425c-8618-beb6a854051a/ITResource.xsd mspl.xsd"
-                                          })
-    
-    itresource = ET.SubElement(itresource_orchestration, "ITResource", 
-                               id="mspl_9e9dbbdfad5d4ef588fc5ec579f83c59", 
-                               orchestrationID="omspl_5b0fd7359a5e40fe92399710c1152ed9")
+    try:
+        itresource_orchestration = ET.Element("ITResourceOrchestration", 
+                                              xmlns="http://modeliosoft/xsddesigner/a22bd60b-ee3d-425c-8618-beb6a854051a/ITResource.xsd",
+                                              attrib={
+                                                  'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+                                                  'id': "omspl_5b0fd7359a5e40fe92399710c1152ed9",
+                                                  'xsi:schemaLocation': "http://modeliosoft/xsddesigner/a22bd60b-ee3d-425c-8618-beb6a854051a/ITResource.xsd mspl.xsd"
+                                              })
 
-    configuration = ET.SubElement(itresource, "configuration", {"xsi:type": "RuleSetConfiguration"})
+        itresource = ET.SubElement(itresource_orchestration, "ITResource", 
+                                   id="mspl_9e9dbbdfad5d4ef588fc5ec579f83c59", 
+                                   orchestrationID="omspl_5b0fd7359a5e40fe92399710c1152ed9")
 
-    capability = ET.SubElement(configuration, "capability")
-    ET.SubElement(capability, "Name").text = json_data["type"].upper()
+        # Configuration element
+        configuration = ET.SubElement(itresource, "configuration", {"xsi:type": "RuleSetConfiguration"})
 
-    configuration_rule = ET.SubElement(configuration, "configurationRule")
-    
-    configuration_rule_action = ET.SubElement(configuration_rule, "configurationRuleAction", {"xsi:type": "DNSACTION"})
-    ET.SubElement(configuration_rule_action, "dnsActionType").text = "Rate"
+        # Capability element
+        capability = ET.SubElement(configuration, "capability")
+        ET.SubElement(capability, "Name").text = "DNS" 
 
-    configuration_condition = ET.SubElement(configuration_rule, "configurationCondition", {"xsi:type": "DNSCondition"})
-    ET.SubElement(configuration_condition, "isCNF").text = "false"
+        # ConfigurationRule element
+        configuration_rule = ET.SubElement(configuration, "configurationRule")
 
-    dns_rate_parameters = ET.SubElement(configuration_condition, "dnsRateParameters")
-    ET.SubElement(dns_rate_parameters, "operation").text = "RATE"
-    ET.SubElement(dns_rate_parameters, "ip").text = json_data["Source"]
-    
-    # Verificar si el campo 'rate' está presente en el JSON, si no, establecer un valor predeterminado de 1
-    rate_value = json_data.get("rate", "1")
-    ET.SubElement(dns_rate_parameters, "rate").text = str(rate_value)
-    
-    external_data = ET.SubElement(configuration_rule, "externalData", {"xsi:type": "Priority"})
-    
-    ET.SubElement(external_data, "value").text = "60000"
+        configuration_rule_action = ET.SubElement(configuration_rule, "configurationRuleAction", {"xsi:type": "DNSACTION"})
+        ET.SubElement(configuration_rule_action, "dnsActionType").text = "Rate"
 
-    ET.SubElement(configuration_rule, "Name").text = "Rule0"
-    ET.SubElement(configuration_rule, "isCNF").text = "false"
-    ET.SubElement(configuration, "Name").text = "Conf0"
+        configuration_condition = ET.SubElement(configuration_rule, "configurationCondition", {"xsi:type": "DNSCondition"})
+        ET.SubElement(configuration_condition, "isCNF").text = "false"
 
-    return ET.tostring(itresource_orchestration, encoding="utf-8", method="xml").decode()
+        dns_rate_parameters = ET.SubElement(configuration_condition, "dnsRateParameters")
+        ET.SubElement(dns_rate_parameters, "operation").text = "RATE" # json_data["if"]["action"]["type"].upper() == RATE LIMIT
+        
+        element_interface = json_data["if"]["element"]["interface"]
+        if element_interface == "*":
+            ET.SubElement(dns_rate_parameters, "ip").text = "0.0.0.0"
+        else:
+            ET.SubElement(dns_rate_parameters, "ip").text = element_interface
 
-@app.route('/')
-def index():
-    app.logger.info('Policy Translator is running')
-    return 'Policy Translator is running', 200
+        ET.SubElement(dns_rate_parameters, "rate").text = json_data["if"]["action"]["value"]
 
-@app.route('/generate/xml', methods=['POST'])
+        external_data = ET.SubElement(configuration_rule, "externalData", {"xsi:type": "Priority"})
+        ET.SubElement(external_data, "value").text = "60000"
+
+        ET.SubElement(configuration_rule, "Name").text = "Rule0"
+        ET.SubElement(configuration_rule, "isCNF").text = "false"
+
+        # # Name element in Configuration
+        ET.SubElement(configuration, "Name").text = "Conf0"
+
+        # EnforcementRequirements element
+        enforcement_requirements = ET.SubElement(itresource, "enforcementRequirements")
+        conf_type = ET.SubElement(enforcement_requirements, "confType")
+        conf_only = ET.SubElement(conf_type, "confOnly")
+        conf_only.text = "true"
+        instance_id = ET.SubElement(conf_type, "instanceID")
+        instance_id.text = "dns-s1"
+
+        return ET.tostring(itresource_orchestration, encoding="utf-8", method="xml").decode()
+    except KeyError as e:
+        print(f"KeyError in json_to_xml: {str(e)}")
+        raise
+
+def create_output_template(input_value, interface_value):
+    output_template = {
+        "id": "1235",
+        "topology_name": "horse_ddos",
+        "attack": "DDoS",
+        "what": {
+            "KPIs": {
+                "element": {
+                    "node": "dns-c",
+                    "interface": interface_value
+                },
+                "metric": "packets-per-second",
+                "result": {
+                    "value": input_value,
+                    "unit": "packets-per-second"
+                }
+            }
+        }
+    }
+    return output_template
+
+@app.route('/api', methods=['POST'])
 def api():
     json_data = request.get_json()
     if not json_data:
@@ -70,6 +106,37 @@ def api():
         return jsonify({"error": f"Validating MSPL translation: {str(e)}"}), 400
     
     return Response(xml_data, mimetype='application/xml')
+
+@app.route('/send_policy', methods=['POST'])
+def send_policy():
+    try:
+        json_data = request.get_json()
+        if not json_data:
+            return jsonify({"error": "Invalid JSON"}), 400
+        
+        try:
+            xml_data = json_to_xml(json_data)
+        except KeyError as e:
+            return jsonify({"error": f"Missing key: {str(e)}"}), 400
+
+        response = requests.post('http://155.54.95.84:8002/meservice', data=xml_data, headers={'Content-Type': 'application/xml', 'Cache-Control': 'no-cache'})
+
+        if 200 <= response.status_code < 300:
+            try:
+                input_value = json_data["if"]["action"]["value"]
+                interface_value = json_data["if"]["element"]["interface"]
+                output_json = create_output_template(input_value, interface_value)
+                return Response(response=json.dumps(output_json), status=200, mimetype='application/json')
+            except KeyError as e:
+                print(f"Error extracting key from JSON: {str(e)}")
+                return jsonify({"error": f"Missing key in input JSON: {str(e)}"}), 400
+        else:
+            print(f"Orchestrator response status code: {response.status_code}")
+            return Response(response=json.dumps({"error": "Failed to send XML to orchestrator"}), status=500, mimetype='application/json')
+    
+    except Exception as e:
+        print(f"Error decoding JSON: {str(e)}")
+        return jsonify({"error": f"Failed to decode JSON object: {str(e)}"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5005)
